74395726ecd790fad53af815f9025c535b81ac3b
Akk5250 <Akkumar52505250@gmail.com>
2025-03-22 10:14:33 -0400

    Created 'SideBar.java'

---
+++ app/src/main/java/org/sciborgs1155/dashboard/BorderNodes/SideBar/SideBar.java
@@ -0,0 +1,10 @@
+package org.sciborgs1155.dashboard.BorderNodes.SideBar;
+
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+public class SideBar extends VBox {
+    public SideBar(Stage stage) {
+
+    }
+}
---
+++ app/src/main/java/org/sciborgs1155/dashboard/BorderNodes/SideBar/SideBarButton.java
@@ -0,0 +1,9 @@
+package org.sciborgs1155.dashboard.BorderNodes.SideBar;
+
+import javafx.scene.control.Button;
+
+public class SideBarButton extends Button {
+    public SideBarButton(SideBar sideBar) {
+
+    }
+}
